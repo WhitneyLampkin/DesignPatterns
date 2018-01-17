c2a829030079aaa96ac225f654d49584685b085a
Whitney Lampkin <wlampkin.davis@htomail.com>
2018-01-17 11:59:22 -0600

    Init

---
+++ ConsoleApplication/StrategyPattern/Ducks/Duck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesignPatterns.StrategyPattern
+{
+    /// <summary>
+    /// Duck superclass 
+    /// </summary>
+    public abstract class Duck
+    {
+        /// <summary>
+        /// Declare two reference variables for the behavior interface types.
+        /// All duck subclasses (in the same package) inherit these.
+        /// </summary>
+        protected IFlyBehavior flyBehavior;
+        protected IQuackBehavior quackBehavior;
+
+        public Duck()
+        {
+
+        }
+
+        public abstract void display();
+
+        public void performFly()
+        {
+            flyBehavior.fly();
+        }
+
+        public void swim()
+        {
+            Console.Write("All ducks float, even decoys!");
+        }
+    }
+}
---
+++ ConsoleApplication/StrategyPattern/Ducks/MallardDuck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesignPatterns.StrategyPattern
+{
+    /// <summary>
+    /// Mallard duck subclass
+    /// </summary>
+    public class MallardDuck : Duck
+    {
+        /// <summary>
+        /// A MallardDuck uses the Quack class to handle its quack,
+        /// so when performQuack is called, the responsibility for
+        /// the quack is delegated to the Quack object and we get a real quack.
+        /// And it uses FlyWithWings as its IFlyBehavior.
+        /// </summary>
+        public MallardDuck()
+        {
+            // These two instance variables are inherited from the Duck superclass.
+            quackBehavior = new Quack();
+            flyBehavior = new FlyWithWings();
+        }
+
+        public override void display()
+        {
+            Console.WriteLine("I'm a real Mallard duck.");
+        }
+    }
+}
